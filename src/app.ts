a61e901359f06021bf4c332d22fe1451a0de397a
KickAps <flo.berson06@gmail.com>
2024-01-28 19:42:44 +0100

    first multiply test

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+class Calculator {
+    number_1: number;
+    number_2: number;
+
+    constructor() {
+        document.addEventListener("DOMContentLoaded", () => {
+            const self = this;
+            const result = (document.querySelector('input#result') as HTMLInputElement);
+
+            self.reset();
+
+            result.addEventListener('keyup', function (e) {
+                if (e.key === 'Enter') {
+                    if (parseInt(this.value) === (self.number_1 * self.number_2)) {
+                        self.reset();
+                    } else {
+                        console.log("nuuul");
+                    }
+                }
+            });
+        });
+    }
+
+    reset() {
+        const input_1 = (document.querySelector('input#number_1') as HTMLInputElement);
+        const input_2 = (document.querySelector('input#number_2') as HTMLInputElement);
+
+        this.number_1 = this.rand(10);
+        this.number_2 = this.rand(10);
+
+        input_1.value = this.number_1.toString();
+        input_2.value = this.number_2.toString();
+    }
+
+    rand(max: number) {
+        return Math.floor(Math.random() * max) + 1;
+    }
+}
+
+new Calculator();
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,19 @@ let path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/app.js',
+    entry: './src/app.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'public/build')
